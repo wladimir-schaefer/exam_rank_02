70de361ab5c4ad811ecbce8a6d25a6eba4afbb8d
wladimir-schaefer <wladimir.schaefer.de@gmail.com>
2025-08-05 22:55:12 +0200

    bits

--- Level_2/reverse_bits.c
+++ Level_2/reverse_bits.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 
-unsigned char	reverse_bits(unsigned char octet)
-{
-	int		i = 8;
-	unsigned char	res = 0;
+// unsigned char	reverse_bits(unsigned char octet)
+// {
+// 	int		i = 8;
+// 	unsigned char	res = 0;
 
-	while (i > 0)
+// 	while (i--)
+// 	{
+// 		res = res * 2 + (octet % 2);
+// 		octet = octet >> 1;
+// 	}
+// 	return (res);
+// }
+
+unsigned char reverse_bits(unsigned char octet)
+{
+	unsigned char res = 0;
+	int i = 8;
+
+	while (i--)
 	{
-		res = res * 2 + (octet % 2);
-		octet = octet / 2;
-		i--;
+		res = (res << 1) | (octet & 1);
+		octet >>= 1;
 	}
-	return (res);
+	return res;
 }
+
 int main()
 {
-	printf("%d\n", reverse_bits(2));
+	printf("%d\n", reverse_bits(70));
 }
